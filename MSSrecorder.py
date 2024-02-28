fa86e8ab532da177730eb668d2ea5dbbcf25d8fe
Drawdecais <97838525+Drawdecais@users.noreply.github.com>
2024-02-28 11:56:41 -0800

    Add files via upload
    ffmpeg setpts

--- MSSrecorder.py
+++ MSSrecorder.py
@@ -1,28 +1,33 @@
-#https://github.com/Drawdecais/MSSrecorder
-import cv2, mss, numpy, pyautogui, keyboard, time
+#https://github.com/Drawdecais/MSSrecorder/blob/main/MSSrecorder.py
+import cv2, mss, numpy, pyautogui, keyboard, time, subprocess
 
+fps = 60
 images = [] #Array para ingresar las imágenes
-tiempo = time.time() #Tiempo inicio del programa
 size = pyautogui.size() #obtener tamaño del monitor
-formato = cv2.VideoWriter_fourcc(*'h264') #formato del vídeo
+formato = cv2.VideoWriter_fourcc(*"x264") #formato del vídeo
 
 #video donde se ingresaran las imágenes
-video = cv2.VideoWriter('video.mp4', formato, 25, (size.width, size.height))
-
+video = cv2.VideoWriter('video.mp4', formato, fps, (size.width, size.height))
 
 with mss.mss() as sct:
-	#top y left indican la posición donde capturará
-	#width y height el tamaño del capture
+	#top y left indican la posición donde capturará, width y height el tamaño del capture
 	monitor = {"top":0, "left":0, "width": size.width, "height": size.height}
-	#inicia un bucle infinito
-	while True:
+	tiempo = time.time() #Tiempo inicio la grabación
+	while True: #inicia un bucle infinito
 		cap = sct.grab(monitor) #capture de mss
 		img = numpy.array(cap) #convertir en array
 		images.append(img) #ingresar en el array
 		if keyboard.is_pressed('q'):break #terminar bucle al precionar 'q'
 
+#imprimir el tiempo que duró grabando, este sera el tiempo que debe tener el video final
+print("Loanding...","time:",time.localtime(round((time.time()-tiempo),2)).tm_sec)
 #convertir todas las imágenes en video
 for image in images:video.write(image)
 video.release()
-#imprimir el tiempo que duró grabando
-print(time.localtime(round((time.time() - tiempo),2)).tm_sec)
+"""Este bucle es porque por alguna razon la primera vez que ffmpeg
+abre el video da error y la segunda vez si funciona"""
+for i in range(3):
+	#setpts=2*PTS relentiza en video manteniendo los 60fps
+	if subprocess.run(f'ffmpeg -loglevel error -stats -i video.mp4 \
+		-filter:v "setpts={fps/28}*PTS" -c:v libx264 -crf 21 -preset slow video2.mp4 -y',
+		 shell=True, capture_output=False).returncode == 0:break
